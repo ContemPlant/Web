c733f66851de43dbcf603905091ff9a665ba459f
Imke Dungs <imke.dungs@campus.tu-berlin.de>
2018-06-24 19:59:07 +0200

    add error handling to Login page

---
+++ src/Components/messages/InlineError.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+// display the message (string) in light red (#ae5856)
+const InlineError = ({ text }) => <span style={{ color: "#ae5856" }}>{text}</span>;
+
+export default InlineError;
--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import { createApolloFetch } from 'apollo-fetch'
-import { Input, Segment, Button, Divider } from 'semantic-ui-react'
+import { Form, Segment, Button, Divider } from 'semantic-ui-react'
+import Validator from 'validator';
+
 import { loginQuery } from '../Utils/queries'
+import InlineError from '../Components/messages/InlineError'
 
 // Connecting to Graphql Endpoint
 const uri = 'http://167.99.240.197:8000/graphql'
@@ -15,65 +18,134 @@ export default class Login extends React.Component {
 
     // Module state
     state = {
+      data: {
         email: '',
         password: ''
-    }
+      },
+      loading: false,
+      errors: {}
+    };
 
     //Changing state on user input
-    onChange = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value,
-        })
-    }
+    onChange = e => this.setState({
+      data: { ...this.state.data, [e.target.name]: e.target.value }
+    });
 
-    // Login function
+
     onLogin = async () => {
+      const errors = this.validate(this.state.data);
+      this.setState({ errors });
+      if (Object.keys(errors).length === 0) {
+        this.setState({ loading: true });
 
         try {
+          fetch({
+            query: loginQuery(this.state.data.email, this.state.data.password),
+          })
+          .then(res => {
 
-            // Fetching query
-            fetch({
-                query: loginQuery(this.state.email, this.state.password),
-            }).then(res => {
-                //      alert("Successfully logged in!")
-                sessionStorage.email = this.state.email
-                sessionStorage.username = res.data.login.user.username
-                sessionStorage.jwt = res.data.login.token
-                this.props.history.push("/overview")
-            })
-        } catch (e) {
-            console.log(e.message)
-            alert(e.message)
-        }
+            if(res.errors) {
+              var errorMsg = res.errors[0].message ? res.errors[0].message : "An error occured";
+              console.log(errorMsg);
+              throw new Error(errorMsg)
+            }
+
+            sessionStorage.email = this.state.data.email
+            sessionStorage.username = res.data.login.user.username
+            sessionStorage.jwt = res.data.login.token
+            this.props.history.push("/overview")
+          })
+          .catch(errorMsg => {
+            this.setState({ loading: false })
+            alert(errorMsg)
+          }
+        );
+      } catch (e) {
+        console.log(e)
+        alert(e)
+      }
+    }
+  }
+
+
+    validate = (data) => {
+      const errors = {};
+      if (!Validator.isEmail(data.email)) errors.email = "Invalid email";
+      if (!data.password) errors.password = "Password can't be blank";
+      return errors;
     }
 
     // Render function
     render() {
+
+        const { data, errors, loading } = this.state;
+
         return (
 
             <center>
                 <div style={{ maxWidth: 400, height: 600 }}>
                     <br />
                     <br />
+                    <div className="ui centered grid container">
                     <Segment padded>
                         <h1>Login</h1>
-                        <br></br>
-                        <Divider horizontal>Email</Divider>
-                        <br></br>
-                        <Input name="email" placeholder="email@email.com" onChange={e => this.onChange(e)} value={this.state.email} />
-                        <br></br>
-                        <Divider horizontal>Password</Divider>
-                        <br></br>
-                        <Input name="password" placeholder="Your password" type="password" onChange={e => this.onChange(e)} value={this.state.password} />
-                        <br></br>
-                        <br></br>
-                        <Button color="green" fluid onClick={() => this.onLogin()}> login </Button>
-                        <br></br>
+                        <br />
+                        <br />
+                        <Form
+                          onSubmit={ ()=> this.onLogin()}
+                          loading={loading}>
+                          <Form.Field error={!!errors.email}>
+                            <label htmlFor="email">
+                              <Divider horizontal>Email</Divider>
+                            </label>
+                            <input
+                              type="email"
+                              id="email"
+                              name="email"
+                              placeholder="example@example.com"
+                              value={data.email}
+                              onChange={this.onChange}
+                              />
+                            {errors.email &&
+                              <InlineError text={errors.email} />
+                            }
+                          </Form.Field>
+                          <Form.Field error={!!errors.password}>
+                            <label htmlFor="password">
+                              <Divider horizontal>Password</Divider>
+                            </label>
+                            <input
+                              type="password"
+                              id="password"
+                              name="password"
+                              placeholder="Your password"
+                              value={data.password}
+                              onChange={this.onChange}
+                              />
+                            {errors.password &&
+                              <InlineError text={errors.password} />
+                            }
+                          </Form.Field>
+                          <Button color="green" fluid >
+                            <i className="unlock alternate icon">
+                            </i>
+                            Login
+                          </Button>
+                        </Form>
+                        <br />
                     </Segment>
-                    <br></br>
+                    <br />
                     <Divider horizontal>Not registered yet?</Divider>
-                    <br></br>
-                    <Button color="orange" fluid onClick={() => { this.props.history.push("/register") }}>Sign up</Button>
+                    <br />
+                    <Button
+                      color="orange"
+                      fluid
+                      onClick={() => { this.props.history.push('/register') }}>
+                      <i className="pencil icon">
+                      </i>
+                      Sign up
+                    </Button>
+                </div>
                 </div>
             </center>
 
