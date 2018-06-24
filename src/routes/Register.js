fdedc587373847707e238e825472f36910307b73
Imke Dungs <imke.dungs@campus.tu-berlin.de>
2018-06-24 20:29:07 +0200

    improve error handling on signup page

--- src/routes/Register.js
+++ src/routes/Register.js
@@ -1,96 +1,172 @@
 import React from 'react'
 import { gql, graphql } from 'react-apollo'
-import {
-    Input,
-    Segment,
-    Button,
-    Divider
-} from 'semantic-ui-react'
+import { Form, Segment, Button, Divider } from 'semantic-ui-react'
+import Validator from 'validator'
 
-//Register React-component
+import InlineError from '../Components/messages/InlineError'
+
+
 class Register extends React.Component {
+  state = {
+    data: {
+      username: '',
+      email: '',
+      password: '',
+      password2: ''
+    },
+    loading: false,
+    errors: {}
+  };
 
-    //State declaration
-    state = {
-        username: '',
-        email: '',
-        password: '',
-        password2: ''
-    }
 
-    //Changing state on user input
-    onChange = (e) => {
+  onChange = e => this.setState({
+    data: { ...this.state.data, [e.target.name]: e.target.value }
+  });
 
-        this.setState({
-            [e.target.name]: e.target.value,
-        })
-    }
 
-    //Register user and persist on DB -> redirect to Login page
     onSubmit = async () => {
-        if (this.state.password !== "" && this.state.password2 !== "" && this.state.email !== "") {
-            if (this.state.password === this.state.password2) {
-                try {
-                    const response = await this.props.mutate({
-                        variables: this.state
-                    })
-                    console.log(response)
-                    //                    alert("User successfully created.")
-                    this.props.history.push("/login")
-                } catch (e) {
-                    alert(e.message)
-                }
-            } else {
-                alert("Passwords must be equal!")
-            }
-        } else {
-            alert("Fields can't be blank!")
+      const errors = this.validate(this.state.data);
+      this.setState({ errors });
+      if (Object.keys(errors).length === 0) {
+        this.setState({ loading: true });
+        try {
+          const response = await this.props.mutate({ variables: this.state.data });
+          console.log(response);
+          alert("User successfully created.")
+          sessionStorage.email = this.state.data.email
+          sessionStorage.username = response.data.signup.user.username
+          sessionStorage.jwt = response.data.signup.token
+          this.props.history.push("/overview")
+        } catch (e) {
+          console.log(e.message);
+          this.setState({ loading: false })
+          alert(e.message);
         }
-    }
+      }
+    };
 
-    //Render function of component
+  validate = (data) => {
+    const errors = {};
+    if (!data.username) errors.username = "Choose a username!"
+    if (!Validator.isEmail(data.email)) errors.email = "Invalid email!";
+    if (!data.password) errors.password = "Password can't be blank!";
+    if (!data.password2 || data.password !== data.password2) errors.password2 = "Passwords have to be equal!";
+    return errors;
+  }
+
+
     render() {
-        return (
-            < center>
-                <div style={{ maxWidth: 400, height: 600 }}>
-                    <br />
-                    <br />
-                    <Segment padded>
-                        <h1>Sign up</h1>
-                        <br></br>
-                        <Divider horizontal>Username</Divider>
-                        <br></br>
-                        <Input name='username' placeholder='username' onChange={e => this.onChange(e)} value={this.state.username} />
-                        <br></br>
-                        <Divider horizontal>Email</Divider>
-                        <br></br>
-                        <Input name='email' placeholder='email@email.com' onChange={e => this.onChange(e)} value={this.state.email} />
-                        <br></br>
-                        <Divider horizontal>Password</Divider>
-                        <br></br>
-                        <Input name='password' placeholder='Choose a password' type='password' onChange={e => this.onChange(e)} value={this.state.password} />
-                        <br></br>
-                        <Input name='password2' placeholder='Repeat password' type='password' onChange={e => this.onChange(e)} value={this.state.password2} />
-                        <br></br>
-                        <br></br>
-                        <Button color='green' fluid onClick={() => this.onSubmit()} type="primary" > Sign up </Button>
-                        <br></br>
-                    </Segment>
-                    <br></br>
-                    <Divider horizontal>Already registered?</Divider>
-                    <br></br>
-                    <Button color='orange' fluid onClick={() => { this.props.history.push("/login") }}>Login</Button>
-                </div>
-            </center>
-        )
-    }
+
+      const { data, errors, loading } = this.state;
+
+      return (
+        < center>
+        <div style={{ maxWidth: 400, height: 600 }}>
+          <br />
+          <br />
+          <Segment padded>
+            <h1>
+              Sign up
+            </h1>
+            <br />
+            <br />
+            <Form
+              onSubmit={ ()=> this.onSubmit()}
+              loading={loading}>
+              <Form.Field error={!!errors.username}>
+                <label htmlFor="username">
+                  <Divider horizontal>Username</Divider>
+                </label>
+                <input
+                  type="text"
+                  id="username"
+                  name="username"
+                  placeholder="Choose a username"
+                  value={data.username}
+                  onChange={this.onChange}
+                  />
+                {errors.username &&
+                  <InlineError text={errors.username} />
+                }
+              </Form.Field>
+              <Form.Field error={!!errors.email}>
+                <label htmlFor="email">
+                  <Divider horizontal>Email</Divider>
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="example@example.com"
+                  value={data.email}
+                  onChange={this.onChange}
+                  />
+                {errors.email &&
+                  <InlineError text={errors.email} />
+                }
+              </Form.Field>
+              <Form.Field error={!!errors.password}>
+                <label htmlFor="password">
+                  <Divider horizontal>Password</Divider>
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  placeholder="Choose a password"
+                  value={data.password}
+                  onChange={this.onChange}
+                  />
+                {errors.password &&
+                  <InlineError text={errors.password} />
+                }
+              </Form.Field>
+              <Form.Field error={!!errors.password2}>
+                <label htmlFor="password2"></label>
+                <input
+                  type="password"
+                  id="password2"
+                  name="password2"
+                  placeholder="Repeat your password"
+                  value={data.password2}
+                  onChange={this.onChange}
+                  />
+                {errors.password2 &&
+                  <InlineError text={errors.password2} />
+                }
+              </Form.Field>
+
+              <Button color="green" fluid >
+                <i className="pencil icon">
+                </i>
+                Sign up
+              </Button>
+            </Form>
+            <br />
+          </Segment>
+          <br />
+          <Divider horizontal>
+            Already registered?
+          </Divider>
+          <br />
+          <Button
+            color='orange'
+            fluid
+            onClick={() => { this.props.history.push("/login") }}>
+            <i className="unlock alternate icon"></i>
+            Login
+          </Button>
+        </div>
+      </center>
+    )
+  }
 }
 
 const mutation = gql`
-    mutation signup ($username: String!, $email: String!, $password: String!){
-            signup(username: $username, email: $email, password:$password) {
-            user{username,id,email}
-    }
+mutation signup ($username: String!, $email: String!, $password: String!){
+  signup(username: $username, email: $email, password:$password) {
+    user{username,id,email}
+  }
 }`
 
 export default graphql(mutation)(Register)
