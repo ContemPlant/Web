9e21aa9ff736d6932bab51e8e90be98dd759e984
Leonard Eyer <le0nard@me.com>
2018-06-29 19:06:01 +0200

    Modify config

--- src/Utils/config.js
+++ src/Utils/config.js
@@ -1,7 +1,7 @@
-const BACKEND_IP = '167.99.240.197:8000'
-const GRAPHQL_URI = `http://${BACKEND_IP}/graphql`
+const BACKEND_IP = '167.99.240.197'
+const PORT = '8000'
+const GRAPHQL_URI = `http://${BACKEND_IP}:${PORT}/graphql`
 
 module.exports = {
-    BACKEND_IP,
     GRAPHQL_URI
 }
